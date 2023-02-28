65cbe5cdb6fc185f67b5abf437536d83ece9eec2
Jan Kočka <kockahonza@gmail.com>
2023-02-28 22:45:05 +0100

    fixed bug in 'lucass_cuts'

--- genie_analysis_lucass_cuts.cpp
+++ genie_analysis_lucass_cuts.cpp
@@ -5,6 +5,8 @@
 int main(int argc, char *argv[]) {
     GenieAnalysisOriginalCuts ga{"/home/honza/Sync/University/CurrentCourses/SHP/data/Genie_gst_2000000.root",
                                  "output_lucass_cuts_allcuts.root",
+                                 GenieAnalysisOriginalCuts::Target::C12,
+                                 GenieAnalysisOriginalCuts::BeamEnergy::MeV_2261,
                                  {"nocut"},
                                  {"W", "el_smeared_mag", "el_smeared_phi", "el_smeared_cos_theta", "el_acceptance"},
                                  {"ALL", "QE", "RES", "DIS"}};
