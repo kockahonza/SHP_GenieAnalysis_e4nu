be89e0e8852edc4a7a2cc0001975eb106ef07591
Jan Kočka <kockahonza@gmail.com>
2023-03-17 18:16:01 +0100

    Added resc value study class

--- genie_analysis_FS_transparency.cpp
+++ genie_analysis_FS_transparency.cpp
@@ -6,6 +6,66 @@
 #include "GenieAnalysis/GenieAnalysisDeltaStudies.h"
 #include "GenieAnalysis/misc.h"
 
+class GenieAnalysisFSTransparency : public GenieAnalysisDeltaStudies {
+  protected:
+    bool m_resc_all_same;
+
+    bool m_resc_all_mone;
+    bool m_resc_all_zero;
+    bool m_resc_all_one;
+
+    map<string, AutoProperty> m_new_known_properties{
+        {"resc_same", {"Are all resc values the same", {2, -0.5, 1.5}, [this]() { return m_resc_all_same; }}},
+        {"resc_all_mone", {"Are all resc values -1", {2, -0.5, 1.5}, [this]() { return m_resc_all_mone; }}},
+        {"resc_all_zero", {"Are all resc values 0", {2, -0.5, 1.5}, [this]() { return m_resc_all_zero; }}},
+        {"resc_all_one", {"Are all resc values 1", {2, -0.5, 1.5}, [this]() { return m_resc_all_one; }}},
+    };
+
+  public:
+    GenieAnalysisFSTransparency(const char *filename, const char *output_filename, const vector<string> &stages = {},
+                                const vector<string> &properties = {}, const vector<string> &types = {},
+                                const vector<GenieAnalysisAutoHistograms::AutoVsPlot> &vs_property_plots = {},
+                                const Target &target = Target::C12,
+                                const BeamEnergy &beam_energy = BeamEnergy::MeV_2261,
+                                const char *gst_ttree_name = "gst")
+        : GenieAnalysis(filename, gst_ttree_name), GenieAnalysisDeltaStudies{
+                                                       filename, output_filename,   stages, properties,
+                                                       types,    vs_property_plots, target, beam_energy} {
+        m_known_properties.insert(m_new_known_properties.begin(), m_new_known_properties.end());
+    }
+
+    Double_t passesCuts() override {
+        Double_t weight{GenieAnalysisDeltaStudies::passesCuts()};
+
+        if (weight == 0) {
+            return 0;
+        }
+
+        m_resc_all_same = true;
+        for (int i{1}; i < m_ge.ni; i++) {
+            if (m_ge.resc[i] != m_ge.resc[0]) {
+                m_resc_all_same = false;
+            }
+        }
+
+        m_resc_all_mone = false;
+        m_resc_all_zero = false;
+        m_resc_all_one = false;
+
+        if (m_resc_all_same) {
+            if (m_ge.resc[0] == -1) {
+                m_resc_all_mone = true;
+            } else if (m_ge.resc[0] == 0) {
+                m_resc_all_zero = true;
+            } else if (m_ge.resc[0] == 1) {
+                m_resc_all_one = true;
+            }
+        }
+
+        return weight;
+    }
+};
+
 int main(int argc, char *argv[]) {
     string input_file, output_file;
 
@@ -49,20 +109,20 @@ int main(int argc, char *argv[]) {
     /*                               }}; */
     /* ga1.runAnalysis(); */
 
-    GenieAnalysisDeltaStudies ga1{input_file.c_str(),
-                                  (output_file + "_FSt_resc.root").c_str(),
-                                  {},
-                                  {},
-                                  {},
-                                  {{"W", "Ws", {}},
-                                   {"fs_num_pip", "nfpip", {}},
-                                   {"fs_num_protons", "nfp", {}},
-                                   {"resc", "passed_num_pip", {}},
-                                   {"resc", "passed_num_pim", {}},
-                                   {"resc", "passed_num_protons", {}},
-                                   {"resc", "fs_num_pip", {}},
-                                   {"resc", "fs_num_pim", {}},
-                                   {"resc", "fs_num_protons", {}}}};
+    GenieAnalysisFSTransparency ga1{input_file.c_str(),
+                                    (output_file + "_FSt_resc.root").c_str(),
+                                    {},
+                                    {},
+                                    {},
+                                    {{"W", "Ws", {}},
+                                     {"fs_num_pip", "nfpip", {}},
+                                     {"fs_num_protons", "nfp", {}},
+                                     {"resc", "passed_num_pip", {}},
+                                     {"resc", "passed_num_pim", {}},
+                                     {"resc", "passed_num_protons", {}},
+                                     {"resc", "fs_num_pip", {}},
+                                     {"resc", "fs_num_pim", {}},
+                                     {"resc", "fs_num_protons", {}}}};
     ga1.runAnalysis();
 
     return 0;
