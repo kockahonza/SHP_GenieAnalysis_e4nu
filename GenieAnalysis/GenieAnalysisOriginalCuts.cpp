5a40f5ce46142a07096ff063127f5c7d70a802b5
Jan Kočka <kockahonza@gmail.com>
2023-03-19 16:32:40 +0100

    Something got messed up in git

---
+++ GenieAnalysis/GenieAnalysisOriginalCuts.cpp
@@ -0,0 +1,201 @@
+#include "GenieAnalysisOriginalCuts.h"
+#include <TMath.h>
+
+Double_t GenieAnalysisOriginalCuts::passesCuts() {
+    useEntryAtStage("nocut", m_ge.wght);
+
+    // Do electron smearing
+    const double smeared_pl{gRandom->Gaus(m_ge.pl, m_smearing_reso_el * m_ge.pl)};
+    const double smeared_El{sqrt(smeared_pl * smeared_pl + e_mass * e_mass)};
+    m_smeared_el_V3.SetXYZ(smeared_pl / m_ge.pl * m_ge.pxl, smeared_pl / m_ge.pl * m_ge.pyl,
+                           smeared_pl / m_ge.pl * m_ge.pzl);
+    m_smeared_el_V4.SetPxPyPzE(m_smeared_el_V3.X(), m_smeared_el_V3.Y(), m_smeared_el_V3.Z(), smeared_El);
+
+    // GENIE coordinate system flipped with respect to CLAS, all the 4momentas phis are added pi to -- blindly taken
+    // from original
+    m_smeared_el_V3.SetPhi(m_smeared_el_V3.Phi() + TMath::Pi());
+
+    // Electron theta and momentum fiducial (essentially I think) cut, the values are specifically for C12 2.261GeV
+    // set by inspecting
+    // https://docs.google.com/presentation/d/1ghG08JfCYXRXh6O8hcXKrhJOFxkAs_9i5ZfoIkiiEHU/edit?usp=sharing and
+    // previous values
+    if (m_do_precuts) {
+        const double theta_min{TMath::DegToRad() * (m_precut_parameter1 + m_precut_parameter2 / m_smeared_el_V3.Mag())};
+        if ((m_smeared_el_V3.Theta() < theta_min) || (m_smeared_el_V3.Theta() < 0 * TMath::DegToRad()) ||
+            (m_smeared_el_V3.Theta() > 80 * TMath::DegToRad())) {
+            return 0;
+        }
+        useEntryAtStage("p_gdoc", m_ge.wght);
+    }
+
+    // This was originally later on but I think it makes more sense here
+    if (m_do_electron_fiducials) {
+        if (!m_fiducials->electronCut(m_smeared_el_V3)) {
+            return 0;
+        }
+        useEntryAtStage("p_efid", m_ge.wght);
+    }
+
+    // Filter some specific sectors, this was enabled and probably makes some sense, essentially only
+    // use sectors 0, 1 and 5, o this only if m_do_sectors is true
+    // Sectors are a bit weird, the first goes from -30 to 30 and the rest go as expected to 330
+    if (m_do_sectors) {
+        double temp{(m_smeared_el_V3.Phi() + TMath::Pi() / 6)};
+        if (temp < 0) {
+            temp += 2 * TMath::Pi();
+        }
+        const int ElectronSector = temp / (TMath::Pi() / 3);
+        if ((ElectronSector >= 2) && (ElectronSector <= 4)) {
+            return 0;
+        }
+    }
+
+    // Calculation of kinematic quantities (nu, Q2, x bjorken, q and W) -- mostly taken from original though
+    // specific to 2.261Gev beam
+    // TODO: Unusued now, delete or use
+    /* const TLorentzVector el_change{m_smeared_el_V4 - TLorentzVector{0, 0, 2.261, 2.261}}; */
+    /* const double reco_Q2 = -el_change.Mag2(); */
+
+    /* const double nu = -el_change.E(); */
+    /* const double x_bjk = reco_Q2 / (2 * m_prot * nu); */
+
+    // Get the electron acceptance weight from the e2a map
+    m_electron_acceptance_weight = electronAcceptance(smeared_pl, m_smeared_el_V3.CosTheta(), m_smeared_el_V3.Phi());
+    if (m_electron_acceptance_weight != TMath::Abs(m_electron_acceptance_weight)) {
+        throw "Electron acceptance not reasonable";
+    }
+
+    // Hadron loop, first clear the vectors though
+    m_passed_pi_minus.clear();
+    m_passed_pi_plus.clear();
+    m_passed_photons.clear();
+
+    // Temp variables for the loop, declared here for performance
+    double smeared_p;
+    double smeared_E;
+    double pi_acceptance;
+    TVector3 V3;
+    double positive_phi_difference;
+    for (int i{0}; i < m_ge.nf; i++) {
+        // pi-
+        if (m_ge.pdgf[i] == -211) {
+            // required momentum for detection
+            if (m_ge.pf[i] < m_p_pion_momentum_threshold) {
+                continue;
+            }
+            smeared_p = gRandom->Gaus(m_ge.pf[i], m_smearing_reso_pi * m_ge.pf[i]);
+            smeared_E = sqrt(smeared_p * smeared_p + m_pion * m_pion);
+
+            V3.SetXYZ(smeared_p / m_ge.pf[i] * m_ge.pxf[i], smeared_p / m_ge.pf[i] * m_ge.pyf[i],
+                      smeared_p / m_ge.pf[i] * m_ge.pzf[i]);
+            V3.SetPhi(V3.Phi() + TMath::Pi());
+
+            if (m_do_pion_fiducials) {
+                if (!m_fiducials->piAndPhotonCuts(V3, FiducialWrapper::PiPhotonId::Minus)) {
+                    continue;
+                }
+            }
+
+            // And only use pirns with reasonable acceptance, quite often they are negative or so on, this is kindof a
+            // workaround
+            pi_acceptance = piMinusAcceptance(V3.Mag(), V3.CosTheta(), V3.Phi());
+            if (pi_acceptance == TMath::Abs(pi_acceptance)) {
+                m_passed_pi_minus.push_back({{V3, smeared_E}, V3, pi_acceptance});
+            }
+        }
+        // pi+
+        if (m_ge.pdgf[i] == 211) {
+            // required momentum for detection
+            if (m_ge.pf[i] < m_p_pion_momentum_threshold) {
+                continue;
+            }
+            smeared_p = gRandom->Gaus(m_ge.pf[i], m_smearing_reso_pi * m_ge.pf[i]);
+            smeared_E = sqrt(smeared_p * smeared_p + m_pion * m_pion);
+
+            V3.SetXYZ(smeared_p / m_ge.pf[i] * m_ge.pxf[i], smeared_p / m_ge.pf[i] * m_ge.pyf[i],
+                      smeared_p / m_ge.pf[i] * m_ge.pzf[i]);
+            V3.SetPhi(V3.Phi() + TMath::Pi());
+
+            if (m_do_pion_fiducials) {
+                if (!m_fiducials->piAndPhotonCuts(V3, FiducialWrapper::PiPhotonId::Plus)) {
+                    continue;
+                }
+            }
+
+            // And only use pirns with reasonable acceptance, quite often they are negative or so on, this is kindof a
+            // workaround
+            pi_acceptance = piMinusAcceptance(V3.Mag(), V3.CosTheta(), V3.Phi());
+            if (pi_acceptance == TMath::Abs(pi_acceptance)) {
+                m_passed_pi_plus.push_back({{V3, smeared_E}, V3, piPlusAcceptance(V3.Mag(), V3.CosTheta(), V3.Phi())});
+            }
+        }
+        // photons
+        if (m_ge.pdgf[i] == 22) {
+            // required momentum for detection
+            if (m_ge.pf[i] < m_p_photon_momentum_threshold) {
+                continue;
+            }
+
+            // No photon smearing
+            V3.SetXYZ(m_ge.pxf[i], m_ge.pyf[i], m_ge.pzf[i]);
+            V3.SetPhi(V3.Phi() + TMath::Pi());
+
+            if (m_do_photon_fiducials) {
+                if (!m_fiducials->piAndPhotonCuts(V3, FiducialWrapper::PiPhotonId::Photon)) {
+                    continue;
+                }
+            }
+
+            // TODO: The following should cut on presence of radiation electrons but it's worth reviewing, a comment
+            // in original says "within 40 degrees in theta and 30 in phi" but that's not what it did, even after
+            // fixing a clear(ish) bug For the second condition there's a phi angle difference the +2pi is to bring
+            // it to the 0 to 4pi range adn then mod 2pi and compare to
+            if (m_do_radiation_check) {
+                positive_phi_difference = TMath::Abs(V3.Phi() - m_smeared_el_V3.Phi()) * TMath::RadToDeg();
+                if ((V3.Angle(m_smeared_el_V3) < m_p_radiation_photon_angle) &&
+                    ((positive_phi_difference < m_p_radiation_photon_phi_diff) ||
+                     (positive_phi_difference > (360 - m_p_radiation_photon_phi_diff)))) {
+                    return 0;
+                }
+            }
+
+            m_passed_photons.push_back({{V3, m_ge.Ef[i]}, V3, photonAcceptance(V3.Mag(), V3.CosTheta(), V3.Phi())});
+        }
+    }
+
+    return m_electron_acceptance_weight * m_ge.wght; // I'm pretty sure all the events have wght=1
+}
+
+double GenieAnalysisOriginalCuts::acceptanceJoined(const double &p, const double &cos_theta, double phi,
+                                                   const std::unique_ptr<TH3D> &generated,
+                                                   const std::unique_ptr<TH3D> &accepted) {
+    // first map -pi, pi to [0, 2pi
+    if (phi < 0) {
+        phi += 2 * TMath::Pi();
+    }
+    phi *= TMath::RadToDeg();
+    // map 330 till 360 to [-30:0] but in radians for the acceptance map histogram
+    if (phi > (330)) {
+        phi -= 360;
+    }
+
+    int redef = 0; // or -30 I think, it was this way in the original
+
+    // Find number of generated events
+    double pbin_gen = generated->GetXaxis()->FindBin(p);
+    double tbin_gen = generated->GetYaxis()->FindBin(cos_theta);
+    double phibin_gen = generated->GetZaxis()->FindBin(phi + redef);
+    double num_gen = generated->GetBinContent(pbin_gen, tbin_gen, phibin_gen);
+
+    // Find number of accepted events
+    double pbin_acc = accepted->GetXaxis()->FindBin(p);
+    double tbin_acc = accepted->GetYaxis()->FindBin(cos_theta);
+    double phibin_acc = accepted->GetZaxis()->FindBin(phi + redef);
+    double num_acc = accepted->GetBinContent(pbin_acc, tbin_acc, phibin_acc);
+
+    double acc_ratio = num_acc / num_gen;
+    // TODO: Unused, use or delete soon
+    /* double acc_err = sqrt(acc_ratio * (1 - acc_ratio)) / num_gen; */
+
+    return acc_ratio;
+}
---
+++ GenieAnalysis/GenieAnalysisOriginalCuts.h
@@ -0,0 +1,252 @@
+#ifndef GENIE_ANALYSIS_ORIGINAL_CUTS_H
+#define GENIE_ANALYSIS_ORIGINAL_CUTS_H
+
+#include <TH3D.h>
+#include <TLorentzVector.h>
+#include <TMath.h>
+#include <TRandom.h>
+#include <memory>
+
+#include "GenieAnalysisAuto.h"
+#include "misc.h"
+
+class FiducialWrapper;
+
+using std::unique_ptr;
+
+// So far I'm only focusing on exactly the stuff used with the command line arguments in the README,
+// specifically this means 2.261GeV beam on C12 target and more -- not quite anymore
+class GenieAnalysisOriginalCuts : public GenieAnalysisAutoTH1Fs {
+  public:
+    // Configuration options for the major target/energy runs
+    enum class Target { C12, Fe56 };
+    enum class BeamEnergy { MeV_1161, MeV_2261, MeV_4461 };
+
+    const Target m_target;
+    const BeamEnergy m_beam_energy;
+
+  public:
+    // Some parameters to be set by directly accessing them, otherwise the constructor would be massive,
+    // the values here are used for the CLAS6 C12 2GeV GENIE data.
+    bool m_do_precuts{true};
+    bool m_do_electron_fiducials{true};
+    bool m_do_pion_fiducials{true};
+    bool m_do_photon_fiducials{true};
+    bool m_do_sectors{false};
+    bool m_do_radiation_check{false};
+
+    double m_p_pion_momentum_threshold{0.15};
+    double m_p_photon_momentum_threshold{0.3};
+    double m_p_radiation_photon_angle{40};
+    double m_p_radiation_photon_phi_diff{30};
+
+  protected:
+    // Automatically determined parameters -- should be essentially const, but leaving mutable for easy initialization
+    // and maybe someone would like to change them
+    double m_smearing_reso_el; // smearing for the electrons
+    double m_smearing_reso_pi; // smearing for pions, executive decision by Larry (28.08.19)
+    double m_smearing_reso_p;  // smearing for the proton -- comments from original
+
+    double m_precut_parameter1;
+    double m_precut_parameter2;
+
+  private:
+    // Paths and other system stuff
+
+    const unique_ptr<FiducialWrapper> m_fiducials;
+
+    // e2a acceptance maps
+    const unique_ptr<TFile> m_el_acceptance_file;
+    const unique_ptr<TFile> m_p_acceptance_file;
+    const unique_ptr<TFile> m_pip_acceptance_file;
+    const unique_ptr<TFile> m_pim_acceptance_file;
+    // It seems to be necessary for I suppose performance reason? that these are found beforehand and not at each call
+    // to acceptanceJoined
+    const unique_ptr<TH3D> m_acc_el_gen, m_acc_el_acc, m_acc_p_gen, m_acc_p_acc, m_acc_pip_gen, m_acc_pip_acc,
+        m_acc_pim_gen, m_acc_pim_acc;
+
+  protected:
+    // Properties of the loaded event to be set and used in passesCuts and new properties
+    // electron
+    TVector3 m_smeared_el_V3; // Smeared and rotated by pi
+    TLorentzVector m_smeared_el_V4;
+    double m_electron_acceptance_weight;
+
+    // hadrons -- all of these only contain information on particles passing relevant cuts (pions need momentum above
+    // 0.15 for example)
+    vector<tuple<TLorentzVector, TVector3, double>>
+        m_passed_pi_plus; // tuple has the smeared 4 momentum, smeared 3 momentum and the pions calculated acceptance
+    vector<tuple<TLorentzVector, TVector3, double>>
+        m_passed_pi_minus; // tuple has the smeared 4 momentum, smeared 3 momentum and the pions calculated acceptance
+    vector<tuple<TLorentzVector, TVector3, double>>
+        m_passed_photons; // tuple has the smeared 4 momentum, smeared 3 momentum and the pions calculated acceptance
+
+    // Extensions to automatic TH1Fs
+    map<string, AutoProperty> m_new_known_properties{
+        {"el_phi",
+         {"Out electron phi [°]",
+          {720, -30, 330},
+          [this]() {
+              double phi_deg{m_smeared_el_V3.Phi() * TMath::RadToDeg()};
+              return (phi_deg < -30) ? phi_deg + 360 : phi_deg;
+          }}},
+        {"el_cos_theta", {"Out electron cos theta", {720, -1, 1}, [this]() { return m_smeared_el_V3.CosTheta(); }}},
+        {"el_p", {"Out electron momentum [GeV/c]", {720, 0, 3}, [this]() { return m_smeared_el_V4.P(); }}},
+        {"el_E", {"Out electron energy [GeV]", {720, 0, 3}, [this]() { return m_smeared_el_V4.Energy(); }}},
+        {"el_acceptance",
+         {"Out electron acceptance weight", {100, 0, 1}, [this]() { return m_electron_acceptance_weight; }}}};
+
+  public:
+    GenieAnalysisOriginalCuts(const char *filename, const char *output_filename,
+                              // Select run
+                              const Target &target, const BeamEnergy &beam_energy,
+                              // Specify the analysis - which stages, properties and types to do histograms for
+                              const vector<string> &stages, const vector<string> &properties,
+                              const vector<string> &types,
+                              // Smearing parameters
+                              const double &smearing_reso_el = 0.005, const double &smearing_reso_pi = 0.007,
+                              const double &smearing_reso_p = 0.01,
+                              // Pass this to GenieAnalysis
+                              const char *gst_ttree_name = "gst")
+        : GenieAnalysisAutoTH1Fs(filename, output_filename, stages, properties, types, gst_ttree_name),
+          m_target{target}, m_beam_energy{beam_energy},
+
+          m_smearing_reso_el{smearing_reso_el}, m_smearing_reso_pi{smearing_reso_pi},
+          m_smearing_reso_p{smearing_reso_p},
+
+          m_fiducials{std::make_unique<FiducialWrapper>(m_target, m_beam_energy)},
+
+          // Initializing acceptance map files and TH3Ds
+          m_el_acceptance_file{getAcceptanceMapFile(m_target, m_beam_energy, "")},
+          m_p_acceptance_file{getAcceptanceMapFile(m_target, m_beam_energy, "_p")},
+          m_pip_acceptance_file{getAcceptanceMapFile(m_target, m_beam_energy, "_pip")},
+          m_pim_acceptance_file{getAcceptanceMapFile(m_target, m_beam_energy, "_pim")},
+          m_acc_el_gen{(TH3D *)m_el_acceptance_file->Get("Generated Particles")},
+          m_acc_el_acc{(TH3D *)m_el_acceptance_file->Get("Accepted Particles")},
+          m_acc_p_gen{(TH3D *)m_p_acceptance_file->Get("Generated Particles")},
+          m_acc_p_acc{(TH3D *)m_p_acceptance_file->Get("Accepted Particles")},
+          m_acc_pip_gen{(TH3D *)m_pip_acceptance_file->Get("Generated Particles")},
+          m_acc_pip_acc{(TH3D *)m_pip_acceptance_file->Get("Accepted Particles")},
+          m_acc_pim_gen{(TH3D *)m_pim_acceptance_file->Get("Generated Particles")},
+          m_acc_pim_acc{(TH3D *)m_pim_acceptance_file->Get("Accepted Particles")}
+
+    {
+        m_known_properties.insert(m_new_known_properties.begin(), m_new_known_properties.end());
+
+        // Initialize precut parameters and also multiply smearing resos by 3 for the ~1GeV as it was in the original
+        if (beam_energy == BeamEnergy::MeV_1161) {
+            m_precut_parameter1 = 17;
+            m_precut_parameter2 = 7;
+
+            m_smearing_reso_el *= 3;
+            m_smearing_reso_pi *= 3;
+            m_smearing_reso_p *= 3;
+        } else if (beam_energy == BeamEnergy::MeV_2261) {
+            m_precut_parameter1 = 16;
+            m_precut_parameter2 = 10.5;
+        } else if (beam_energy == BeamEnergy::MeV_4461) {
+            m_precut_parameter1 = 13.5;
+            m_precut_parameter2 = 15;
+        }
+    }
+
+    Double_t passesCuts();
+
+    double acceptanceJoined(const double &p, const double &cos_theta, double phi, const unique_ptr<TH3D> &generated,
+                            const unique_ptr<TH3D> &accepted);
+
+    double electronAcceptance(const double &p, const double &cos_theta, const double &phi) {
+        return acceptanceJoined(p, cos_theta, phi, m_acc_el_gen, m_acc_el_acc);
+    }
+
+    double photonAcceptance(const double &p, const double &cos_theta, const double &phi) {
+        return acceptanceJoined(p, cos_theta, phi, m_acc_p_gen, m_acc_p_acc);
+    }
+
+    double piPlusAcceptance(const double &p, const double &cos_theta, const double &phi) {
+        return acceptanceJoined(p, cos_theta, phi, m_acc_pip_gen, m_acc_pip_acc);
+    }
+
+    double piMinusAcceptance(const double &p, const double &cos_theta, const double &phi) {
+        return acceptanceJoined(p, cos_theta, phi, m_acc_pim_gen, m_acc_pim_acc);
+    }
+
+    static unique_ptr<TFile> getAcceptanceMapFile(const Target &target, const BeamEnergy &beam_energy,
+                                                  const string &ending) {
+        string target_str, beam_energy_str;
+        if (target == Target::C12) {
+            target_str = "12C";
+        } else if (target == Target::Fe56) {
+            target_str = "12C"; // There's no dedicated Fe file and original used 12C for anything except He
+        }
+
+        if (beam_energy == BeamEnergy::MeV_1161) {
+            beam_energy_str = "1_161";
+        } else if (beam_energy == BeamEnergy::MeV_2261) {
+            beam_energy_str = "2_261";
+        } else if (beam_energy == BeamEnergy::MeV_4461) {
+            beam_energy_str = "4_461";
+        }
+
+        return unique_ptr<TFile>(TFile::Open(
+            ("original/e2a_maps/e2a_maps_" + target_str + "_E_" + beam_energy_str + ending + ".root").c_str(), "READ"));
+    };
+};
+
+// A very simple wrapper around the Fiducial class, taken exactly as in original, so that it can be referred to in a
+// more modern way
+class FiducialWrapper {
+  public:
+    enum class PiPhotonId : int { Minus = -1, Photon = 0, Plus = 1 };
+
+    const GenieAnalysisOriginalCuts::Target m_target;
+    const GenieAnalysisOriginalCuts::BeamEnergy m_beam_energy;
+    const string m_target_str;
+    const string m_beam_energy_str;
+
+  private:
+    Fiducial m_fiducial;
+
+  public:
+    FiducialWrapper(const GenieAnalysisOriginalCuts::Target &target,
+                    const GenieAnalysisOriginalCuts::BeamEnergy &beam_energy)
+        : m_target(target), m_beam_energy(beam_energy), m_target_str{targetStr(m_target)},
+          m_beam_energy_str{beamEnergyStr(m_beam_energy)}, m_fiducial{} {
+
+        m_fiducial.InitPiMinusFit(m_beam_energy_str);
+        m_fiducial.InitEClimits();
+
+        // The first value is torusCurrent, values taken from original
+        m_fiducial.SetConstants(m_beam_energy == GenieAnalysisOriginalCuts::BeamEnergy::MeV_1161 ? 750 : 2250,
+                                m_target_str, {{"1161", 1.161}, {"2261", 2.261}, {"4461", 4.461}});
+        m_fiducial.SetFiducialCutParameters(m_beam_energy_str);
+    }
+
+    bool electronCut(const TVector3 &momentum_V3) { return m_fiducial.EFiducialCut(m_beam_energy_str, momentum_V3); }
+
+    bool piAndPhotonCuts(const TVector3 &momentum_V3, const PiPhotonId &which_particle) {
+        return m_fiducial.Pi_phot_fid_united(m_beam_energy_str, momentum_V3, static_cast<int>(which_particle));
+    }
+
+    static string targetStr(const GenieAnalysisOriginalCuts::Target &target) {
+        if (target == GenieAnalysisOriginalCuts::Target::C12) {
+            return "12C";
+        } else if (target == GenieAnalysisOriginalCuts::Target::Fe56) {
+            return "12C"; // There's no dedicated Fe file and original used 12C for anything except He
+        }
+        throw "This should not happen";
+    }
+
+    static string beamEnergyStr(const GenieAnalysisOriginalCuts::BeamEnergy &beam_energy) {
+        if (beam_energy == GenieAnalysisOriginalCuts::BeamEnergy::MeV_1161) {
+            return "1161";
+        } else if (beam_energy == GenieAnalysisOriginalCuts::BeamEnergy::MeV_2261) {
+            return "2261";
+        } else if (beam_energy == GenieAnalysisOriginalCuts::BeamEnergy::MeV_4461) {
+            return "4461";
+        }
+        throw "This should not happen";
+    }
+};
+
+#endif
